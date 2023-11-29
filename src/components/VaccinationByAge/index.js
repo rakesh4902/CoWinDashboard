f49c7073e22d4935cd37b1680dd7f96050665087
rakesh4902 <modir810@gmail.com>
2023-11-29 16:24:23 +0000

    Solution

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -8,30 +8,28 @@ const VaccinationByAge = props => {
   return (
     <div className="vaccination-by-gender-cont">
       <h1 className="vaccination-by-gender-heading">Vaccination by Age</h1>
-      <div className="pie-cont">
-        <PieChart width={100} height={300}>
-          <Pie
-            cx="50%"
-            cy="30%"
-            data={vaccinationByAgeDetails}
-            startAngle={0}
-            endAngle={360}
-            outerRadius="60%"
-            dataKey="count"
-          >
-            <Cell name="18-44" fill=" #2d87bb" />
-            <Cell name="44-60" fill="#a3df9f" />
-            <Cell name="Above 60" fill="#64c2a6" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="horizontal"
-            verticalAlign="bottom"
-            align="center"
-            wrapperStyle={{fontSize: 12, fontFamily: 'Roboto'}}
-          />
-        </PieChart>
-      </div>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="30%"
+          data={vaccinationByAgeDetails}
+          startAngle={0}
+          endAngle={360}
+          outerRadius="60%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill=" #2d87bb" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#64c2a6" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="horizontal"
+          verticalAlign="bottom"
+          align="center"
+          wrapperStyle={{fontSize: 12, fontFamily: 'Roboto'}}
+        />
+      </PieChart>
     </div>
   )
 }
